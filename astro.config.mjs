1803817581d821d70b1723d95223be6046cd9cc2
Paul MAZEAU <mazeau.paul@gmail.com>
2024-04-16 15:26:56 +0200

    accueil et paeg video

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
   output: "server",
   adapter: vercel()
 });
